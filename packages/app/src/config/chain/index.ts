8a43d588a8f6b1f2f93b876ee992d03d48255f68
Tawnee <tawnee.a@proton.me>
2024-12-05 11:30:35 -0700

    remove unused config

--- packages/app/src/config/chain/index.ts
+++ packages/app/src/config/chain/index.ts
@@ -1,13 +1,10 @@
 import { getOriginChainId } from '@/domain/hooks/useOriginChainId'
-import { baseSavingsInfoQueryOptions } from '@/domain/savings-info/baseSavingsInfo'
 import { useStore } from '@/domain/state'
 import { CheckedAddress } from '@/domain/types/CheckedAddress'
 import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
 import { TokenSymbol } from '@/domain/types/TokenSymbol'
 import { assets } from '@/ui/assets'
-import { base } from 'viem/chains'
 import { AppConfig } from '../feature-flags'
-import { PLAYWRIGHT_USDS_CONTRACTS_NOT_AVAILABLE_KEY } from '../wagmi/config.e2e'
 import { hyperTestnet } from './constants'
 import { ChainConfigEntry, ChainMeta, SupportedChainId } from './types'
 import { USDXL_ADDRESS } from '../consts'
@@ -21,9 +18,6 @@ const commonTokenSymbolToReplacedName = {
   [TokenSymbol('WETH')]: { name: 'Ethereum', symbol: TokenSymbol('ETH') },
   [TokenSymbol('weETH')]: { name: 'Ether.fi Staked ETH', symbol: TokenSymbol('weETH') },
 }
-
-const _PLAYWRIGHT_MAINNET_USDS_CONTRACTS_NOT_AVAILABLE =
-  import.meta.env.VITE_PLAYWRIGHT === '1' && (window as any)[PLAYWRIGHT_USDS_CONTRACTS_NOT_AVAILABLE_KEY] === true
 
 const chainConfig: Record<SupportedChainId, ChainConfigEntry> = {
   [hyperTestnet.id]: {
@@ -77,7 +71,7 @@ const chainConfig: Record<SupportedChainId, ChainConfigEntry> = {
       oracles: {
         [TokenSymbol('WHYPE')]: {
           type: 'market-price',
-          providedBy: ['chainlink', 'chronicle'],
+          providedBy: [],
         },
         [TokenSymbol('USDC')]: {
           type: 'fixed',
@@ -93,56 +87,6 @@ const chainConfig: Record<SupportedChainId, ChainConfigEntry> = {
     savings: undefined,
     farms: undefined,
   },
-  ...(typeof import.meta.env.VITE_DEV_BASE_DEVNET_RPC_URL === 'string'
-    ? {
-        [base.id]: {
-          originChainId: base.id as SupportedChainId,
-          daiSymbol: undefined,
-          sdaiSymbol: undefined,
-          usdsSymbol: TokenSymbol('USDS'),
-          susdsSymbol: TokenSymbol('sUSDS'),
-          psmStables: [TokenSymbol('USDC'), TokenSymbol('USDS')],
-          meta: {
-            name: 'Base DevNet',
-            logo: assets.chain.baseDevNet,
-          },
-          tokensWithMalformedApprove: [],
-          permitSupport: {},
-          airdrop: {},
-          extraTokens: [
-            {
-              symbol: TokenSymbol('USDC'),
-              oracleType: 'fixed-usd',
-              address: CheckedAddress('0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'),
-            },
-            {
-              symbol: TokenSymbol('sUSDS'),
-              oracleType: 'ssr-auth-oracle',
-              address: CheckedAddress('0x02Edc8718799a22fCBeBEd0C58a1D09657C81bC8'),
-            },
-            {
-              symbol: TokenSymbol('USDS'),
-              oracleType: 'fixed-usd',
-              address: CheckedAddress('0x21F5b5dF683B6885D6A88f330C4474ADeE2A6ed3'),
-            },
-            {
-              symbol: TokenSymbol('SKY'),
-              oracleType: 'zero-price',
-              address: CheckedAddress('0xA40D3Ad0dEdED3df8cDf02108AFf90220C437B82'),
-            },
-          ] as const,
-          markets: undefined,
-          savings: {
-            savingsDaiInfoQuery: undefined,
-            savingsUsdsInfoQuery: baseSavingsInfoQueryOptions,
-            inputTokens: [TokenSymbol('USDC'), TokenSymbol('USDS')],
-            getEarningsApiUrl: undefined,
-            savingsRateApiUrl: undefined,
-          },
-          farms: undefined,
-        } satisfies ChainConfigEntry,
-      }
-    : {}),
 }
 
 export function getChainConfigEntry(chainId: number): ChainConfigEntry {
