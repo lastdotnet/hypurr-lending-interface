dcbd63224c26601b883428140749a72c622188eb
Tawnee <tawnee.a@proton.me>
2024-12-05 16:08:13 -0700

    use full width success button

--- packages/app/src/features/faucet/views/SuccessView.tsx
+++ packages/app/src/features/faucet/views/SuccessView.tsx
@@ -4,8 +4,10 @@ import { LinkButton } from '@/ui/atoms/button/Button'
 export function SuccessView() {
   return (
     <div>
-      <p className="mb-2">You've received 10 USDC and 10 sUSDe 🎉</p>
-      <LinkButton to={paths.myPortfolio}>Go to portfolio</LinkButton>
+      <p className="mb-4">You've received 10 USDC and 10 sUSDe 🎉</p>
+      <LinkButton className="w-full" to={paths.myPortfolio}>
+        Go to portfolio
+      </LinkButton>
     </div>
   )
 }
