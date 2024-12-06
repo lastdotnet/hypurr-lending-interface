ec41e32c7ee6f15cfcc750260d24a233114aa6d8
Tawnee <tawnee.a@proton.me>
2024-12-06 09:39:54 -0700

    hardcode usdxl in borrow page text

--- packages/app/src/features/easy-borrow/components/BorrowRateBanner.tsx
+++ packages/app/src/features/easy-borrow/components/BorrowRateBanner.tsx
@@ -8,7 +8,7 @@ export interface BorrowRateBannerProps {
 }
 
 export function BorrowRateBanner({ assetsToBorrowMeta }: BorrowRateBannerProps) {
-  const { dai, usds, borrowRate } = assetsToBorrowMeta
+  const { borrowRate } = assetsToBorrowMeta
   return (
     <div className="flex flex-col items-center md:mt-12">
       <div className="flex items-center gap-2">
@@ -18,8 +18,7 @@ export function BorrowRateBanner({ assetsToBorrowMeta }: BorrowRateBannerProps) 
         </Typography>
       </div>
       <Typography variant="h3" className="mt-6 text-center font-medium text-lg text-primary-bg/70 md:text-2xl">
-        Borrow {dai}
-        {usds ? ` or ${usds}` : ''} at <span className="text-white">{formatPercentage(borrowRate)}</span> from Hypurr
+        Borrow USDxl at <span className="text-white">{formatPercentage(borrowRate)}</span> from Hypurr
       </Typography>
     </div>
   )
