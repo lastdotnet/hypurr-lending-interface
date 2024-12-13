dba66a941b33db9846373749ce5955f234fb7148
Tawnee <tawnee.a@proton.me>
2024-12-13 15:51:40 -0700

    remove react helmet from components

--- packages/app/src/pages/Faucet.tsx
+++ packages/app/src/pages/Faucet.tsx
@@ -1,15 +1,5 @@
 import { FaucetContainer } from '@/features/faucet/FaucetContainer'
-import { Helmet } from 'react-helmet'
 
 export function FaucetPage() {
-  return (
-    <>
-      <Helmet>
-        <meta property="og:image" content="https://app.hypurr.fi/faucet-meta.jpg" />
-        <meta name="twitter:image" content="https://app.hypurr.fi/faucet-meta.jpg" />
-      </Helmet>
-
-      <FaucetContainer />
-    </>
-  )
+  return <FaucetContainer />
 }
