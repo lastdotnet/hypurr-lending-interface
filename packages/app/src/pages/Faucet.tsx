7b43a73e0fa4199ce2beb112859f68322aeb295f
Tawnee <tawnee.a@proton.me>
2024-12-13 12:40:43 -0700

    add social links and meta to faucet success. remove sandbox config

--- packages/app/src/pages/Faucet.tsx
+++ packages/app/src/pages/Faucet.tsx
@@ -1,5 +1,15 @@
 import { FaucetContainer } from '@/features/faucet/FaucetContainer'
+import { Helmet } from 'react-helmet'
 
 export function FaucetPage() {
-  return <FaucetContainer />
+  return (
+    <>
+      <Helmet>
+        <meta property="og:image" content="/faucet-meta.jpg" />
+        <meta name="twitter:image" content="/faucet-meta.jpg" />
+      </Helmet>
+
+      <FaucetContainer />
+    </>
+  )
 }
