4107b0b43d9f641cbbc8c361811a5906bf73802b
Tawnee <tawnee.a@proton.me>
2025-01-08 13:13:02 -0700

    update borrow panel title

--- packages/app/tailwind.config.ts
+++ packages/app/tailwind.config.ts
@@ -102,6 +102,9 @@ export default {
         'light-blue': 'rgba(var(--nav-primary) / <alpha-value>)',
         'product-dark-blue': 'rgb(var(--product-dark-blue))',
       },
+      fontSize: {
+        '5.5xl': '3.5rem',
+      },
       boxShadow: {
         nav: '0px 20px 40px 0px var(--nav-shadow)',
         tooltip: '0px 4px 30px 7px var(--tooltip-shadow)',
