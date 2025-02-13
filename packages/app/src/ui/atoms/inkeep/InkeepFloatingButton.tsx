208af815c426ab094c00e86186d647b3900ff6e1
Tawnee <tawnee.a@proton.me>
2025-02-13 08:47:08 -0700

    style button

--- packages/app/src/ui/atoms/inkeep/InkeepFloatingButton.tsx
+++ packages/app/src/ui/atoms/inkeep/InkeepFloatingButton.tsx
@@ -1,12 +1,19 @@
 'use client'
 
 import { useInkeep } from '@/domain/state/inkeep'
+import { Button, buttonVariants } from '@/ui/atoms/button/Button'
+import { cn } from '@/ui/utils/style'
+import { MessagesSquareIcon } from 'lucide-react'
 
 export function InkeepFloatingButton() {
   const { handleOpen } = useInkeep()
   return (
-    <button className="fixed right-4 bottom-4 z-50" onClick={handleOpen}>
+    <Button
+      className={cn(buttonVariants({ variant: 'floating' }), 'fixed right-4 bottom-4 z-50 p-5')}
+      onClick={handleOpen}
+    >
+      <MessagesSquareIcon className="h-4 w-4" />
       Ask AI
-    </button>
+    </Button>
   )
 }
