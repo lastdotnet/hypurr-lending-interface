a8055585208f83e116a772b266f78e34fbb57e5d
Tawnee <tawnee.a@proton.me>
2025-02-14 13:48:34 -0700

    remove unused assets

--- packages/app/src/ui/assets/index.ts
+++ packages/app/src/ui/assets/index.ts
@@ -9,14 +9,7 @@ import exchange from './actions/exchange.svg?url'
 import repay from './actions/repay.svg?url'
 import withdraw from './actions/withdraw.svg?url'
 import arrowRight from './arrow-right.svg?url'
-import daiToUsdsUpgrade from './banners/dai-to-usds-upgrade.webp'
-import mkrToSkyTransform from './banners/mkr-to-sky-transform.svg?url'
-import newSavingsWelcome from './banners/new-savings-welcome.webp'
-import sdaiToSusdsUpgrade from './banners/sdai-to-susds-upgrade.webp'
 import boxArrowTopRight from './box-arrow-top-right.svg?url'
-import baseDevNet from './chains/baseDevNet.svg?url'
-import ethereum from './chains/ethereum.svg?url'
-import gnosis from './chains/gnosis.svg?url'
 import checkCircle from './check-circle.svg?url'
 import chevronDown from './chevron-down.svg?url'
 import circleInfo from './circle-info.svg?url'
@@ -29,11 +22,9 @@ import eye from './eye.svg?url'
 import flash from './flash.svg?url'
 import giftbox from './giftbox.svg?url'
 import greenArrowUp from './green-arrow-up.svg?url'
-import lifiLogo from './lifi-logo.svg?url'
 import link from './link.svg?url'
 import magicWandCircle from './magic-wand-circle.svg?url'
 import magicWand from './magic-wand.svg?url'
-import makerLogo from './maker.svg?url'
 import chart from './markets/chart.svg?url'
 import inputOutput from './markets/input-output.svg?url'
 import lock from './markets/lock.svg?url'
@@ -47,47 +38,19 @@ import pause from './pause.svg?url'
 import rocket from './rocket.svg?url'
 import sliderThumb from './slider-thumb.svg?url'
 import snowflake from './snowflake.svg?url'
-import sparkIcon from './spark-icon.svg?url'
-import sparkLogo from './spark-logo.svg?url'
 import success from './success.svg?url'
 import threeDots from './three-dots.svg?url'
 import timer from './timer.svg?url'
-import cbbtc from './tokens/cbbtc.svg?url'
-import cle from './tokens/cle.svg?url'
-import dai from './tokens/dai.svg?url'
-import eth from './tokens/eth.svg?url'
-import eure from './tokens/eure.svg?url'
-import gno from './tokens/gno.svg?url'
-import mkr from './tokens/mkr.svg?url'
-import reth from './tokens/reth.svg?url'
-import sdai from './tokens/sdai.svg?url'
-import sky from './tokens/sky.svg?url'
 import steth from './tokens/steth.svg?url'
-import susds from './tokens/susds.svg?url'
 import unknown from './tokens/unknown.svg?url'
 import usdc from './tokens/usdc.svg?url'
-import usds from './tokens/usds.svg?url'
 import usdt from './tokens/usdt.svg?url'
-import wbtc from './tokens/wbtc.svg?url'
-import weeth from './tokens/weeth.svg?url'
-import weth from './tokens/weth.svg?url'
-import wsteth from './tokens/wsteth.svg?url'
-import wxdai from './tokens/wxdai.svg?url'
-import xdai from './tokens/xdai.svg?url'
 import khype from './tokens/khype.svg?url'
 import up from './up.svg?url'
 import upgrade from './upgrade.svg?url'
-import coinbase from './wallet-icons/coinbase.svg?url'
-import defaultWallet from './wallet-icons/default.svg?url'
-import enjin from './wallet-icons/enjin.svg?url'
-import metamask from './wallet-icons/metamask.svg?url'
-import torus from './wallet-icons/torus.svg?url'
-import walletConnect from './wallet-icons/wallet-connect.svg?url'
 import wallet from './wallet.svg?url'
 import warning from './warning.svg?url'
 import xCircle from './x-circle.svg?url'
-import lastLogo from './last-logo.svg?url'
-import surrf from './surrf.svg?url'
 import hypurrLogo from './hypurr-logo.svg?url'
 import hypurrPaw from './hypurr-paw.svg?url'
 import hyperEvmLogo from './hyper-evm-logo.png'
@@ -101,17 +64,17 @@ import sttesth from './tokens/sttesth.svg?url'
 import telegram from './social-logos/telegram-logo.svg?url'
 import x from './social-logos/x-logo.svg?url'
 import github from './social-logos/github-logo.svg?url'
+import coinbase from './wallet-icons/coinbase.svg?url'
+import defaultWallet from './wallet-icons/default.svg?url'
+import metamask from './wallet-icons/metamask.svg?url'
+import walletConnect from './wallet-icons/wallet-connect.svg?url'
 
 export const assets = {
   hypurrPaw,
   faucetPurr,
   hyperEvmLogo,
-  surrf,
   hypurrLogo,
   hypurrLogoText,
-  sparkLogo,
-  sparkIcon,
-  lifiLogo,
   chevronDown,
   sliderThumb,
   circleInfo,
@@ -138,18 +101,22 @@ export const assets = {
   menu,
   close,
   closeFilled,
-  makerLogo,
   giftbox,
   rocket,
   downgrade,
   timer,
   multiply,
   equal,
-  lastLogo,
   socialLogos: {
     telegram,
     x,
     github,
+  },
+  walletIcons: {
+    metamask,
+    walletConnect,
+    coinbase,
+    default: defaultWallet,
   },
   markets: {
     chart,
@@ -169,34 +136,13 @@ export const assets = {
     exchange,
   },
   chain: {
-    gnosis,
-    ethereum,
+    hyper: hyperEvmLogo,
     unknown,
-    baseDevNet,
   },
   token: {
-    dai,
-    eth,
-    eure,
-    gno,
-    mkr,
-    sky,
-    usds,
-    reth,
-    sdai,
-    susds,
     steth,
     usdc,
-    'usdc.e': usdc,
     usdt,
-    wbtc,
-    weeth,
-    weth,
-    wsteth,
-    wxdai,
-    xdai,
-    cbbtc,
-    cle,
     usdxl,
     susde,
     hype,
@@ -205,20 +151,6 @@ export const assets = {
     sttesth,
     khype,
     unknown,
-  },
-  walletIcons: {
-    coinbase,
-    enjin,
-    metamask,
-    torus,
-    walletConnect,
-    default: defaultWallet,
-  },
-  banners: {
-    daiToUsdsUpgrade,
-    sdaiToSusdsUpgrade,
-    mkrToSkyTransform,
-    newSavingsWelcome,
   },
   oracleProviders: {
     redstone,
