574be5d0397133b27a4d2620b61a851a2d2fb364
Tawnee <tawnee.a@proton.me>
2025-02-14 08:42:43 -0700

    update radix, remove forward ref from button and input

--- packages/app/src/ui/atoms/button/Button.tsx
+++ packages/app/src/ui/atoms/button/Button.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 
 import { cn } from '@/ui/utils/style'
 import { Link, LinkProps } from '../link/Link'
+import { Ref } from 'react'
 
 export const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-xs border border-slate-700 border-opacity-10 font-semibold text-base ring-offset-background transition-colors disabled:pointer-events-none disabled:bg-slate-700 disabled:bg-opacity-10 disabled:text-white/70 disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
@@ -84,27 +85,40 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = 'Button'
 
 export type LinkButtonProps = VariantProps<typeof buttonVariants> &
-  LinkProps & { disabled?: boolean; prefixIcon?: React.ReactNode; postfixIcon?: React.ReactNode }
+  LinkProps & {
+    disabled?: boolean
+    prefixIcon?: React.ReactNode
+    postfixIcon?: React.ReactNode
+    ref: Ref<HTMLAnchorElement>
+  }
 
-export const LinkButton = React.forwardRef<HTMLAnchorElement, LinkButtonProps>(
-  ({ className, variant, rounded, size, spaceAround, disabled, prefixIcon, postfixIcon, ...props }, ref) => {
-    return (
-      <>
-        {disabled ? (
-          <Button disabled className={cn(buttonVariants({ variant, rounded, size, spaceAround, className }))}>
-            {prefixIcon}
-            {props.children}
-            {postfixIcon}
-          </Button>
-        ) : (
-          <Link className={cn(buttonVariants({ variant, rounded, size, spaceAround, className }))} ref={ref} {...props}>
-            {prefixIcon}
-            {props.children}
-            {postfixIcon}
-          </Link>
-        )}
-      </>
-    )
-  },
-)
-LinkButton.displayName = 'LinkButton'
+export function LinkButton({
+  className,
+  variant,
+  rounded,
+  size,
+  spaceAround,
+  disabled,
+  prefixIcon,
+  postfixIcon,
+  ref,
+  ...props
+}: LinkButtonProps) {
+  return (
+    <>
+      {disabled ? (
+        <Button disabled className={cn(buttonVariants({ variant, rounded, size, spaceAround, className }))}>
+          {prefixIcon}
+          {props.children}
+          {postfixIcon}
+        </Button>
+      ) : (
+        <Link className={cn(buttonVariants({ variant, rounded, size, spaceAround, className }))} ref={ref} {...props}>
+          {prefixIcon}
+          {props.children}
+          {postfixIcon}
+        </Link>
+      )}
+    </>
+  )
+}
