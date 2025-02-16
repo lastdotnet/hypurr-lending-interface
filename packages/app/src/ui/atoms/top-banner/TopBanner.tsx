72e094e62c2746a2184eeab567bfc1f3a784e7c4
Tawnee <tawnee.a@proton.me>
2025-02-15 21:16:27 -0700

    update banner text

--- packages/app/src/ui/atoms/top-banner/TopBanner.tsx
+++ packages/app/src/ui/atoms/top-banner/TopBanner.tsx
@@ -5,14 +5,22 @@ interface TopBannerProps {
   onClose: () => void
 }
 
-export const TOP_BANNER_ID = 'top-banner-2'
+export const TOP_BANNER_ID = 'top-banner-3'
 
 export function TopBanner({ onClose }: TopBannerProps) {
   return (
     <div className="relative top-0 right-0 left-0 z-50 flex w-full flex-col items-center justify-center gap-2 bg-primary-bg p-2 text-center text-body text-sm xl:fixed sm:flex-row">
       <p className="px-10">
-        Due to recent EVM updates, the testnet app has been redeployed and balances zeroed. Please visit our faucet to
-        get more HYPE.
+        Hyperliquid EVM has recently updated their RPC endpoint. Please ensure your wallet is using the{' '}
+        <a
+          href="https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/evm"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          correct URL
+        </a>
+        .
       </p>
 
       <Button variant="icon" size="sm" className="-translate-y-1/2 absolute top-1/2 right-1.5" onClick={onClose}>
