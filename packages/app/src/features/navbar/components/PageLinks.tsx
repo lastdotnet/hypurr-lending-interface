d2f198b2131117ccd04cd3f95dd2107872ae5245
Tawnee <tawnee.a@proton.me>
2025-03-03 12:17:36 -0700

    comment out button (#130)

--- packages/app/src/features/navbar/components/PageLinks.tsx
+++ packages/app/src/features/navbar/components/PageLinks.tsx
@@ -1,9 +1,8 @@
 import { paths } from '@/config/paths'
 import { cn } from '@/ui/utils/style'
-import { FeedbackFish } from '@feedback-fish/react'
 
 import { SavingsInfoQueryResults } from '../types'
-import { focusVariants, NavLink, PlaceholderNavLink, textVariants } from './nav-link/NavLink'
+import { NavLink, PlaceholderNavLink } from './nav-link/NavLink'
 import {
   ArrowRightLeftIcon,
   LayoutGridIcon,
@@ -91,7 +90,7 @@ export function PageLinks({ mobileMenuCollapsed, closeMobileMenu }: PageLinksPro
         ),
       )}
 
-      <FeedbackFish projectId={process.env.NEXT_PUBLIC_FEEDBACK_FISH_PROJECT_ID || ''}>
+      {/* <FeedbackFish projectId={process.env.NEXT_PUBLIC_FEEDBACK_FISH_PROJECT_ID || ''}>
         <button
           className={cn(
             focusVariants(),
@@ -101,7 +100,7 @@ export function PageLinks({ mobileMenuCollapsed, closeMobileMenu }: PageLinksPro
         >
           <MessageSquareIcon className="h-5 w-5" /> Give Feedback
         </button>
-      </FeedbackFish>
+      </FeedbackFish> */}
     </div>
   )
 }
