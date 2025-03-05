ea72154e4307d84b8ff41c56528463237289de60
Tawnee <tawnee.a@proton.me>
2025-03-04 17:05:07 -0700

    add summary styles

--- packages/app/src/features/points/components/summary-panel/SummaryPanel.tsx
+++ packages/app/src/features/points/components/summary-panel/SummaryPanel.tsx
@@ -1,13 +1,41 @@
 import { Panel } from '@/ui/atoms/panel/Panel'
 import { Typography } from '@/ui/atoms/typography/Typography'
+import { PropsWithChildren } from 'react'
+import Image from 'next/image'
+import { assets } from '@/ui/assets'
+import { MoveUpIcon } from 'lucide-react'
 
 export function SummaryPanel() {
   return (
-    <Panel.Wrapper className="flex flex-1 flex-col items-start gap-2 px-9 py-11">
-      <span className="font-medium text-white/50 text-xs">Total Points</span>
-      <Typography variant="h3" gradient>
-        13,109
-      </Typography>
+    <Panel.Wrapper className="flex flex-1 justify-between gap-2 px-9 py-11">
+      <SummaryItem label="Total points">
+        <Typography variant="h3" gradient className="flex items-center gap-1 font-semibold text-3xl">
+          <Image src={assets.hypurrPaw} alt="Points" width={32} height={26} className="block w-8 pt-1" />
+          13,109
+        </Typography>
+      </SummaryItem>
+
+      <SummaryItem label="Ranking">
+        <Typography variant="h3" className="flex items-center font-semibold text-3xl">
+          <MoveUpIcon className="h-5 w-5 text-primary" />
+          18
+        </Typography>
+      </SummaryItem>
+
+      <SummaryItem label="Weeks active">
+        <Typography variant="h3" className="font-semibold text-3xl">
+          5
+        </Typography>
+      </SummaryItem>
     </Panel.Wrapper>
+  )
+}
+
+function SummaryItem({ label, children }: { label: string } & PropsWithChildren) {
+  return (
+    <div className="flex flex-col items-start gap-1">
+      <span className="font-semibold text-white/50 text-xs">{label}</span>
+      {children}
+    </div>
   )
 }
